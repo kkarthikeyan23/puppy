f8589a2b5e8b3473787507b49ca5ada97acc098d
karthikkalam <snehakarthik23@gmail.com>
2024-12-12 12:29:59 -0500

    third Commit

---
+++ 045-mod-code-depend-01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/kkarthikeyan23/puppy"
+)
+
+func main() {
+
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println2(s2)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kkarthikeyan23/puppy
+
+go 1.23.4
