c812434ac19d6eb8cf79527687ca2d24620295bd
karthikkalam <snehakarthik23@gmail.com>
2024-12-12 15:55:49 -0500

    Fourth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kkarthikeyan23/puppy
 
 go 1.23.4
+
+require github.com/kkarthikeyan23/dog v0.0.0-20241212203949-198fea8de66b // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import "github.com/kkarthikeyan23/dog"
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
+
 }
